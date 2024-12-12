0f280030ae66f3239910e3d61a2f1ea5ebe127b9
Artur Signell <artur@vaadin.com>
2024-12-12 18:02:31 +0200

    Use signals transform

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins:[[
+              'module:@preact/signals-react-transform',
+              {
+                mode: 'all', // Needed to include translations which do not use something.value
+              },
+            ]]
+    }
+  })],
 })
